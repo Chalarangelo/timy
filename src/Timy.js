6030e72f593f15d6912a3baa3d2f2a6fc8397819
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-20 19:44:43 +0300

    Finish up code

--- src/Timy.js
+++ src/Timy.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import formatDuration from './formatDuration';
 import useInterval from './useInterval';
+import formatConfig from './formatConfig';
 
 const Timy = ({
-  date
+  date = new Date(),
+  config = formatConfig
 }) => {
-  return (
-    <>
-    </>);
-}
+  const [__date, __setDate] = React.useState(date);
+  const [value, setValue] = React.useState(new Date() - __date);
+
+  useInterval(() => {
+    setValue(new Date() - __date);
+  }, 5000);
+
+  return formatDuration(value, config);
+};
+
+export default Timy;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import formatConfig from './formatConfig';
+import Timy from './Timy';
+
+export { Timy, formatConfig };
